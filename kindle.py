738be7b2a71300d74eb2c6df50bb9a244eb35e66
Andrés M. Jiménez <andresjimenezrios@gmail.com>
2024-01-04 16:02:10 +0100

    fix(kindle): errores

--- kindle.py
+++ kindle.py
@@ -2,24 +2,14 @@
 import re
 import dateparser
 
-
 path_highlights = "files/txt/My Clippings.txt"
 separator = "=========="
 
-highl_kws = 'subrayado|Subrayado' 
+highl_kws = 'subrayado|Subrayado'
 note_kws = 'nota|Nota'
 page_kws = 'página'
-added = 'Añadid. el' 
+added = 'Añadid. el'
 locs = 'posición|Pos\.'
-
-
-def read_clippings_file(path,separator=separator):
-    try:
-        with open(path, encoding="utf-8-sig") as f:
-            return f.read().split(separator)
-    except:
-        print(f"{path} is not accessible. Verify that the path is correct.")
-        quit()
 
 
 def get_highlight_data(hl):
@@ -31,48 +21,65 @@ def get_highlight_data(hl):
     md['title'] = book.group('title').strip()
     md['author'] = book.group('author').strip()
     ### defines if it's a note or a highlight -- So far, it only retrieves those elemets
-    if re.search(highl_kws,cont[1]):
+    if re.search(highl_kws, cont[1]):
         md['type'] = 'highlight'
-    elif re.search(note_kws,cont[1]):
+    elif re.search(note_kws, cont[1]):
         md['type'] = 'note'
     else:
         return None
     ## Location or position in the ebook
-    loc = re.match(r'.*('+ locs + r') (?P<location>[0-9,-]+).*', cont[1])
+    loc = re.match(r'.*(' + locs + r') (?P<location>[0-9,-]+).*', cont[1])
     md['location'] = loc.group('location')
     ## Page
     try:
-        pag = re.match(r'.*('+page_kws+') (?P<page>[0-9,-]+) |$', cont[1])
+        pag = re.match(r'.*(' + page_kws + ') (?P<page>[0-9,-]+) |$', cont[1])
         md['page'] = pag.group('page')
     except:
         md['page'] = ''
     ## Date
-    dateS = re.match(r".*("+added +") (?P<timestamp>.*) (?P<hour>[0-9]+)(H|:)(?P<minutes>[0-9]+)(H|:)(?P<seconds>[0-9]+)('|.)(?P<id>.*)$", cont[1])
+    dateS = re.match(
+        r".*(" + added + ") (?P<timestamp>.*) (?P<hour>[0-9]+)(H|:)(?P<minutes>[0-9]+)(H|:)(?P<seconds>[0-9]+)('|.)(?P<id>.*)$",
+        cont[1])
     date = f"{dateS.group('timestamp')} {dateS.group('hour')}:{dateS.group('minutes')}:{dateS.group('seconds')}"
-    date = date + dateS.group('id') if dateS.group('id') in ['AM','PM'] else date
+    date = date + dateS.group('id') if dateS.group('id') in ['AM', 'PM'] else date
     md['date_time'] = dateparser.parse(date)
     ### Assigns the body of the highlight or note
     md['highlight'] = cont[-1]
     return md
 
 
-def get_highlights(path):
-    highlights_list = read_clippings_file(path)
-    highlights = []
-    print ('Retrieving highlights')
-    for hl in highlights_list:
-        if hl!='\n':
-            md = get_highlight_data(hl)
-            if md:
-                highlights.append(md)
-    print (f'{len(highlights)} highlights retrieved from the original file.')
-    return highlights
+def get_highlights(path=path_highlights, separator=separator, encoding="utf-8-sig"):
+    csv = {}
+    location = {}
 
+    with open(path, encoding=encoding) as kindle_file:
+        highlights_list = kindle_file.read().split(separator)
 
-def main():
-    highlights = get_highlights(path_highlights)
-    print(highlights[-1])
+        for hl in highlights_list:
+            if hl != '\n':
+                md = get_highlight_data(hl)
+                if md:
+                    title = f"[{md['page'].zfill(4)}] {md['highlight'][:150]}"
+
+                    if md['type'] == "highlight":
+                        loc = md['location'].split('-')[-1]
+                        location[loc] = md['date_time']
+                        csv[md['date_time']] = {"title": title, "date": md['date_time'], "text": md['highlight'],
+                                                "author": md['author'], "book": md['title'], "ref": md['page'],
+                                                "tags": ""}
+                    elif md['type'] == "note":
+                        dt = location[loc]
+                        tag = md['highlight'].replace('.', ',')
+                        if not tag[0].isalpha():
+                            tag = tag[1:]
+
+                        if dt in csv:
+                            csv[dt]["tags"] = tag
+                        else:
+                            print(f"Cannot find this location {tag}.")
+
+    return list(csv.values())
 
 
 if __name__ == '__main__':
-    main()
+    print(get_highlights(path_highlights)[16])
--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 from csv import DictReader
 from datetime import datetime
 from joplin import notebook, note, tag, tag_note, jex
+from kindle import get_highlights
 
 LOCATION = (0, 0, 0)
 CREATOR = "SYSTEM"
@@ -19,7 +20,7 @@ def __database_nb(key, obj, fn, args={}):
         return ide
 
 
-def __date(data, pattern):
+def __date(data, pattern=None):
     try:
         res = datetime.strptime(data, pattern)
     except ValueError:
@@ -34,7 +35,7 @@ def __title(title, subtitle):
 
 
 def __metadata(text, row):
-    values = ['date', 'author', 'book', 'ref']
+    values = ['date', 'author', 'book', 'ref', 'tags']
     return text + "\n\n\n-----\n" + "\n".join(['- {}'.format(row[value]) for value in values if row[value]])
 
 
@@ -54,26 +55,39 @@ def csv_process(infile, location=LOCATION, creator=CREATOR, parent=None, metadat
         nb = ini_process() if parent is None else parent
 
         for row in reader:
-            date = __date(row['date'], dateformat)
-            title = __title(row['title'], row['ref'])
-            body = __metadata(row['text'], row) if metadata else row['text']
-
-            author = __database_nb(row['author'], AUTHORS, notebook,
-                                   {'title': row['author'].upper(), 'parent': nb, 'date': date}
-                                   )
-            __database_nb(author, BOOKS, dict)
-            book = __database_nb(row['book'], BOOKS[author], notebook,
-                                 {'title': row['book'], 'parent': author, 'date': date}
-                                 )
-
-            quote = note(
-                title, body, book, date, author=creator,
-                latitude=location[0], longitude=location[1], altitude=location[2]
-            )
-
-            if len(row['tags']):
-                for key in row['tags'].split(','):
-                    keyword = __database_nb(key.strip(), TAGS, tag, {'title': key.strip(), 'date': date})
-                    tag_note(keyword, quote)
+            jex_process(nb, row, location, creator, metadata, dateformat)
 
     return jex(outfile) if outfile else None
+
+
+def kindle_process(infile, location=LOCATION, creator=CREATOR, parent=None, metadata=True, outfile=None):
+    nb = ini_process() if parent is None else parent
+
+    for row in get_highlights(infile):
+        jex_process(nb, row, location, creator, metadata)
+
+    return jex(outfile) if outfile else None
+
+
+def jex_process(nb, row, location, creator, metadata, dateformat=None):
+    date = __date(row['date'], dateformat)
+    title = __title(row['title'], row['ref'])
+    body = __metadata(row['text'], row) if metadata else row['text']
+
+    author = __database_nb(row['author'], AUTHORS, notebook,
+                            {'title': row['author'].upper(), 'parent': nb, 'date': date}
+                            )
+    __database_nb(author, BOOKS, dict)
+    book = __database_nb(row['book'], BOOKS[author], notebook,
+                            {'title': row['book'], 'parent': author, 'date': date}
+                            )
+
+    quote = note(
+        title, body, book, date, author=creator,
+        latitude=location[0], longitude=location[1], altitude=location[2]
+    )
+
+    if len(row['tags']):
+        for key in row['tags'].split(','):
+            keyword = __database_nb(key.strip(), TAGS, tag, {'title': key.strip(), 'date': date})
+            tag_note(keyword, quote)
